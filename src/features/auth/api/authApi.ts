6b264eef5b012f9b2024a2ba2069dbd9b9e017da
Ruslan Pachov <cao-macao@mail.ru>
2026-09-11 16:31:40 +0300

    lesson 4 start

--- src/features/auth/api/authApi.ts
+++ src/features/auth/api/authApi.ts
@@ -1,12 +1,19 @@
-import { baseApi } from "@/app/api/baseApi"
-import type { MeResponse } from "./authApi.types"
+import { baseApi } from "@/app/api/baseApi";
+import type { LoginArgs, LoginResponse, MeResponse } from "./authApi.types";
 
 export const authApi = baseApi.injectEndpoints({
-  endpoints: build => ({
+  endpoints: (build) => ({
     getMe: build.query<MeResponse, void>({
       query: () => `auth/me`,
     }),
+    login: build.mutation<LoginResponse, LoginArgs>({
+      query: (payload) => ({
+        url: `auth/login`,
+        method: "post",
+        body: { ...payload, accessTokenTTL: "3m" },
+      }),
+    }),
   }),
-})
- 
-export const { useGetMeQuery } = authApi
+});
+
+export const { useGetMeQuery } = authApi;
